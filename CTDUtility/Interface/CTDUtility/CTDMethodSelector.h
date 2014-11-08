f1ba2682695c54a7130b7c1c37c13e780dc37ea8
Michael Hackett <mhackett@randomsample.org>
2014-11-08 17:18:21 -0400

    Add a utility class for holding an Obj-C method selector in object form.
    Method selectors in Obj-C are represented by a primitive type (SEL), which is little more than a pointer to static structure built by the compiler and embedded into the application or library code. These cannot be put into Foundation collections without first converting to NSValue or NSString form. The CTDMethodSelector class is little more, but it is more intent-revealing and provides a few useful methods, and is a place to hang other related methods.

---
+++ CTDUtility/Interface/CTDUtility/CTDMethodSelector.h
@@ -0,0 +1,23 @@
+// CTDMethodSelector:
+//     An object representation of an Objective-C method selector (normally a
+//     primitive type, SEL). This is convenient for use with collections and
+//     display. (Sure it's possible to use string representations of method
+//     names, but these will not be automatically updated during refactorings
+//     the way @selector() arguments will be.)
+//
+// Copyright 2014 Michael Hackett. All rights reserved.
+
+
+@interface CTDMethodSelector : NSObject <NSCopying>
+
+@property (assign, readonly, nonatomic) SEL rawSelector;
+
+
+- (instancetype)initWithRawSelector:(SEL)rawSelector;
+CTD_NO_DEFAULT_INIT
+
+- (BOOL)isEqualToRawSelector:(SEL)rawSelector;
+
+// Note: -description returns the selector as a string
+
+@end
